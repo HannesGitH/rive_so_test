80f210cec7df2c4b525b5fcaa757456e909b16b7
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-11-14 17:16:03 +0100

    newer ndk

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 void main() {
   runApp(const MyApp());
@@ -105,6 +106,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const RiveAnimation.network(
+              'https://cdn.rive.app/animations/vehicles.riv',
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
